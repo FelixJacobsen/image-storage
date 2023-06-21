b97ad434a12f1e56209a749102cb5e4f61f784c4
FelixJacobsen <felix.jacobsen@iths.se>
2023-06-21 17:01:09 +0200

    initial commit

---
+++ src/main/java/feljac/tech/imagestorage/ImageStorageApplication.java
@@ -0,0 +1,13 @@
+package feljac.tech.imagestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/feljac/tech/imagestorage/ImageStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package feljac.tech.imagestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
