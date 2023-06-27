c08ab150678dc83ce24bac42a2ac6bfa61e3571f
FelixJacobsen <felix.jacobsen@iths.se>
2023-06-27 21:26:47 +0200

    Change to MongoDBRepository with String as id

--- src/main/java/feljac/tech/imagestorage/repository/ImageRepository.java
+++ src/main/java/feljac/tech/imagestorage/repository/ImageRepository.java
@@ -1,9 +1,9 @@
 package feljac.tech.imagestorage.repository;
 
 import feljac.tech.imagestorage.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface ImageRepository extends MongoRepository<Image, String> {
 }
